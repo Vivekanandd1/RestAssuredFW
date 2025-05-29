a4f4871e4c73fd0e806c48499cf3b18b80ca2c77
vivkekanandd1 <vivekanandd1@live.com>
2025-05-29 08:51:31 +0530

    Added priority and dependsOnMethod

---
+++ src/test/java/BasicRequest/HTTPReq.java
@@ -0,0 +1,63 @@
+package BasicRequest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+public class HTTPReq {
+	int id;
+
+	@Test
+	void getUsers() {
+		given()
+
+				.when().get("https://reqres.in/api/users?page=2")
+
+				.then().statusCode(200).body("page", equalTo(2)).log().all();
+	}
+
+	@Test (priority = 0)
+	void createUser() {
+
+		HashMap<String,String> data = new HashMap<>();
+		data.put("name", "Brod");
+		data.put("job", "Teacher");
+
+		given().header("x-api-key", "reqres-free-v1").contentType("application/json").body(data)
+
+				.when().post("https://reqres.in/api/users").jsonPath().getInt("id");// https://reqres.in/api/users
+
+	}
+    
+	
+	
+	@Test  (priority = 2,dependsOnMethods = "createUser")
+	void UpdateUser() {
+
+		HashMap<String,String> data = new HashMap<>();
+		data.put("name", "Bryan");
+		data.put("job", "StreetRacer");
+
+		given().header("x-api-key", "reqres-free-v1").contentType("application/json").body(data)
+
+				.when().put("https://reqres.in/api/users/" + id)
+
+				.then().statusCode(200).log().all();
+	}
+    
+	
+	
+	@Test (priority = 3)
+	void DeleteUser() {
+
+		given().header("x-api-key", "reqres-free-v1")
+
+				.when().delete("https://reqres.in/api/users/" + id)
+
+				.then().statusCode(204).log().all();
+	}
+
+}
