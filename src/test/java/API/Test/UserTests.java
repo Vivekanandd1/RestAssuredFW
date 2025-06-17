27bd19976a944498070835104d191d9925c1a4d1
vivkekanandd1 <vivekanandd1@live.com>
2025-06-17 21:17:56 +0530

    Framwork setup

---
+++ src/test/java/API/Test/UserTests.java
@@ -0,0 +1,38 @@
+package API.Test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import API.Endpoints.UserEndpoints;
+import API.Payload.User;
+import io.restassured.response.Response;
+
+public class UserTests {
+	
+	Faker faker;
+	User userPayload;
+	
+	@BeforeClass
+	public void setupData() {
+		faker = new Faker();
+		userPayload = new User();
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5,10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+	}
+
+	@Test(priority = 1)
+	public void TestPostUser() {
+		Response response = UserEndpoints.CreateUser(userPayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+}
