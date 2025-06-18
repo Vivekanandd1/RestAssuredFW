1ff12a4e442bd7e3f917208b4fa4cc6d8a997a2d
vivkekanandd1 <vivekanandd1@live.com>
2025-06-18 13:11:58 +0530

    Upgraded endpoints

--- src/test/java/API/Endpoints/UserEndpoints.java
+++ src/test/java/API/Endpoints/UserEndpoints.java
@@ -8,31 +8,31 @@ import static io.restassured.RestAssured.given;
 public class UserEndpoints {
 	
 	public static Response CreateUser(User Payload){
-		Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(Payload)
+		Response response = given().header("api_key", "special-key").contentType(ContentType.JSON).accept(ContentType.JSON).body(Payload)
 		
 		.when().post(Routes.Post_URL);
 		
 		return response;
 	}
 	
-	public static Response ReasdUser(String UserName){
-		Response response = given().pathParam("UserName", UserName)
+	public static Response ReadUser(String username){
+		Response response = given().header("api_key", "special-key").pathParam("username", username)
 		
 		.when().get(Routes.Get_URL);
 		
 		return response;
 	}
 	
-	public static Response UpdateUser(String UserName, User Payload){
-		Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON).pathParam("UserName",UserName).body(Payload)
+	public static Response UpdateUser(String username, User Payload){
+		Response response = given().header("api_key", "special-key").contentType(ContentType.JSON).accept(ContentType.JSON).pathParam("username", username).body(Payload)
 		
-		.when().post(Routes.Update_URL);
+		.when().put(Routes.Update_URL);
 		
 		return response;
 	}
 	
-	public static Response DeleteUser(String UserName){
-		Response response = given().pathParam("UserName", UserName)
+	public static Response DeleteUser(String username){
+		Response response = given().header("api_key", "special-key").pathParam("username", username)
 		
 		.when().delete(Routes.Delete_URL);
 		
