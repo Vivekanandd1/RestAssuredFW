31ab59ad1540d28b3d863ee0e4afa03c2680ed86
vivkekanandd1 <vivekanandd1@live.com>
2025-10-17 20:07:34 +0530

    Bearer Token

---
+++ src/test/java/NewFW/BearerToken.java
@@ -0,0 +1,37 @@
+package NewFW;
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class BearerToken {
+	
+	@Test
+	   public void BearerAuth() {
+		   RequestSpecification res = RestAssured.given();
+
+			res.baseUri("https://gorest.co.in");
+			res.basePath("/public/v2/users");
+			   
+			JSONObject jLoad = new JSONObject();
+			jLoad.put("name", "Viveka");
+			jLoad.put("gender", "male");
+			jLoad.put("email", "vivek@yopmail.com");
+			jLoad.put("status", "active");
+			
+			String AuthToken = "Bearer 9452f8defe2050620fad5ce22cbeb1fda97bf4f192b6fb19e8fbfddfe1280285";
+			
+			Response resp = res.header("Authorization", AuthToken).contentType("application/json").body(jLoad.toString()).post();
+			 
+			System.out.println(resp.statusLine());
+			System.out.println(resp.getBody().asString());
+			
+			
+			
+			
+	   }
+
+}
