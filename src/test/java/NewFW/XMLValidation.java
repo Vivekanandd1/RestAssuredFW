c0b562b4a05b187b8373fee6ab3801c178595e43
vivkekanandd1 <vivekanandd1@live.com>
2025-10-23 15:57:34 +0530

    XML validation and Object creation for Json body

---
+++ src/test/java/NewFW/XMLValidation.java
@@ -0,0 +1,67 @@
+package NewFW;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class XMLValidation {
+
+	@Test
+	public void addPet() {
+		String XMLdata = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+				+ "<Pet>\r\n"
+				+ "	<id>0</id>\r\n"
+				+ "	<Category>\r\n"
+				+ "		<id>0</id>\r\n"
+				+ "		<name>string</name>\r\n"
+				+ "	</Category>\r\n"
+				+ "	<name>doggie</name>\r\n"
+				+ "	<photoUrls>\r\n"
+				+ "		<photoUrl>string</photoUrl>\r\n"
+				+ "	</photoUrls>\r\n"
+				+ "	<tags>\r\n"
+				+ "		<Tag>\r\n"
+				+ "			<id>0</id>\r\n"
+				+ "			<name>string</name>\r\n"
+				+ "		</Tag>\r\n"
+				+ "	</tags>\r\n"
+				+ "	<status>available</status>\r\n"
+				+ "</Pet>";
+		String JsonData = "{\r\n"
+				+ "  \"id\": 0,\r\n"
+				+ "  \"category\": {\r\n"
+				+ "    \"id\": 0,\r\n"
+				+ "    \"name\": \"string\"\r\n"
+				+ "  },\r\n"
+				+ "  \"name\": \"cattie\",\r\n"
+				+ "  \"photoUrls\": [\r\n"
+				+ "    \"string\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"tags\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"id\": 0,\r\n"
+				+ "      \"name\": \"string\"\r\n"
+				+ "    }\r\n"
+				+ "  ],\r\n"
+				+ "  \"status\": \"available\"\r\n"
+				+ "}";
+		RequestSpecification res = RestAssured.given();
+		res.baseUri("https://petstore.swagger.io");
+		res.basePath("/v2/pet");
+		res.header("accept","application/xml");
+		res.body(XMLdata);
+		res.header("Content-TYpe","application/xml");
+		 Response resp = res.post();
+		 resp.prettyPrint();
+		 
+		Assert.assertEquals(resp.statusCode(), 200);
+		resp.then().body("Pet.name", Matchers.equalTo("doggie"));
+		 
+
+	}
+}
