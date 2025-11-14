0b067648449c10ae7edc4af7e23803072fbac713
vivkekanandd1 <vivekanandd1@live.com>
2025-11-14 15:40:20 +0530

    Pojo to array and Back to objects

---
+++ src/test/java/Pojo/Employee.java
@@ -0,0 +1,43 @@
+package Pojo;
+
+public class Employee {
+	
+	String Firstname;
+	String Lastname;
+	int Age;
+	String Gender;
+	double Salary;
+
+	
+	public String getFirstname() {
+		return Firstname;
+	}
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+	public String getLastname() {
+		return Lastname;
+	}
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getGender() {
+		return Gender;
+	}
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+	public double getSalary() {
+		return Salary;
+	}
+	public void setSalary(double salary) {
+		Salary = salary;
+	}
+
+}
